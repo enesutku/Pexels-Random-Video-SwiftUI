99d9cfb279842146ecb5e54289a8d8f26d55af4a
Enes UTKU <ensutku@hotmail.com>
2024-05-11 10:10:51 +0300

    Initial Commit

---
+++ PexelsRandomVideo/PexelsRandomVideoApp.swift
@@ -0,0 +1,12 @@
+// Created by Enes UTKU
+
+import SwiftUI
+
+@main
+struct PexelsRandomVideoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
